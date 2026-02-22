19e62a07edbe1acabf4473c754fda73e3b308b76
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2026-02-22 10:42:19 -0300

    adding a message to see what's inside de input before and after entering data

--- tests/basicinteractions.test.ts
+++ tests/basicinteractions.test.ts
@@ -4,11 +4,13 @@ test("basic interactions", async ({ page }) => {
     await page.goto("https://the-internet.herokuapp.com/inputs");
     const messageInput= page.locator("input[type='number']")
     console.log(await messageInput.getAttribute(""))
+    console.log("Before entering data:" + await messageInput.inputValue());
 
     const headingCss = page.locator('h3:has-text("Inputs")')
     await expect(headingCss).toBeVisible();
 
     await page.fill("input[type='number']", "1234");
+    console.log("After entering data:" + await messageInput.inputValue());
     await page.waitForTimeout(3000);
 
-})  
+})
