1858423a83e3aeaffbb4f0342cfb9d15e3efa335
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2026-03-04 16:13:52 -0300

    changing alert button

--- tests/alert.spec.ts
+++ tests/alert.spec.ts
@@ -4,14 +4,17 @@ test("handling alerts", async ({page}) =>{
     await page.goto('https://demoqa.com/alerts')
 
     page.on('dialog',async (alert)=>{
-        const text = alert.message();
+        //const text = alert.message();
+        const text = alert.defaultValue();
         console.log(text);
-        await alert.accept();
+        await alert.accept("joao");
     })
 
-    const firstAlertButton = page.locator("button[id='confirmButton']");
+    //const firstAlertButton = page.locator("button[id='confirmButton']");
+    const firstAlertButton = page.locator("button[id='promtButton']");
     await firstAlertButton.click();
-    expect(page.locator('span[id="confirmResult"]')).toHaveText("You selected Ok");
+    //expect(page.locator('span[id="confirmResult"]')).toHaveText("You selected Ok");
+    expect(page.locator('span[id="promptResult"]')).toHaveText("You entered joao");
 
     await page.waitForTimeout(3000);
     
